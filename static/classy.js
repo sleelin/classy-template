6537467a42a2a0c7ff53aa0f5b58f26f4067525b
Sam Lee-Lindsay <sleelin@gmail.com>
2021-10-23 01:35:23 +1100

    add(feature): smooth scrolling to targetted article

--- static/classy.js
+++ static/classy.js
@@ -8,6 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// Hook in table of contents smooth scroll behaviour
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById(document.location.hash.substring(1))?.scrollIntoView({behavior: "smooth"});
+    window.addEventListener("hashchange", () => 
+        document.getElementById(document.location.hash.substring(1))?.scrollIntoView({behavior: "smooth"}));
+    
+    let toc = document.querySelector(".content-toc"),
+        links = [
+            ...(document.querySelector(".content-nav")?.querySelectorAll("a") ?? []),
+            ...(toc?.querySelectorAll("a") ?? [])
+        ];
+    
+    for (let a of links) {
+        let target = a.getAttribute("href");
+        
+        a.addEventListener("click", (e) => {
+            e.preventDefault();
+            toc?.removeAttribute("open");
+            document.getElementById(target.substring(1))?.scrollIntoView({behavior: "smooth"});
+            window.history.pushState(null, null, target);
+        });
+    }
+});
+
 // Highlight targeted line in source files 
 document.addEventListener("DOMContentLoaded", () => {
     let source = document.querySelector("#source > .source"),
