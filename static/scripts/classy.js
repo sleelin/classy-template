7539ada4fa3aa03b11853efe8601c19b3cabc152
Sam Lee-Lindsay <sleelin@gmail.com>
2021-10-22 19:41:59 +1100

    refactor(statics): remove further obsolete files and rename assets

--- static/scripts/classy.js
+++ static/scripts/classy.js
@@ -1,3 +1,4 @@
+// Hook in tiny nav toggle's orphaned checkboxes
 document.addEventListener("DOMContentLoaded", () => {
     let source = document.querySelector("#tiny-page-nav-toggle"),
         targets = [...document.querySelectorAll(".tiny-page-nav-target")];
@@ -5,4 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
     source.addEventListener("change", (event) => {
         for (let target of targets) target.checked = event.target.checked;
     });
+});
+
+// Highlight targeted line in source files 
+document.addEventListener("DOMContentLoaded", () => {
+    let source = document.querySelector("#source > .source"),
+        anchorHash = document.location.hash.substring(1),
+        lines = (source ? source.getElementsByTagName("li") : false);
+    
+    if (lines) {
+        for (let i = 0, line = 1; i < lines.length; ++i, ++line) {
+            let lineId = (lines[i].id = `line${line}`);
+            if (lineId === anchorHash) lines[i].classList.add("selected");
+        }
+    }
 });
